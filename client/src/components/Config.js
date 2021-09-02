2efb05c2d9f7498c9b07b6afadd17b02897f9378
이소림 <isolim@isolim-ui-MacBookPro.local>
2021-09-02 14:43:55 +0900

    first

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const API_KEY = "fae22e9b39d3f5e6b8d58fef9fcf49d1";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import { response } from 'express';
+import React , { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import {API_URL, API_KEY} from '../../Config'
+
 
 function LandingPage() {
+    const [Movies, setMovies] = useState([])
+        useEffect(() => {
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+            fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([response.results])
+                
+        })
+    },[])
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{width: '100%', margin: '0'}}>
+        
+        <div style={{ width:'85%', margin: '1rem auto'}}>
+            <h2>Movies by latest</h2>
+            <hr />
+            
+        </div>
+        
+        <div style={{display: 'flex', justifyContent:'center'}}>
+            <button> Load More</button>
+        </div>
     )
-}
+    </div>
+    )}
 
 export default LandingPage
